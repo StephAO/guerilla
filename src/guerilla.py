80d2fc2430a1c268ba594c6176ff7fccd624d076
miguelarocao <mig_ao@live.com>
2016-09-07 16:30:17 -0700

    Wrote player class, fixed game class and tweaked player abstraction

--- src/guerilla.py
+++ src/guerilla.py
@@ -4,12 +4,10 @@ import search
 
 class Guerilla(player.Player):
     
-    def __init__(self, name, colour):
+    def __init__(self, name, colour=None):
         super(Guerilla, self).__init__(name, colour)
         self.nn = neural_net.NeuralNet()
         self.search = search.Search(self.nn.evaluate)
 
     def get_move(self, board):
         return self.search.minimax(board)
-
-player.Player.register(Guerilla)
--- src/player.py
+++ src/player.py
@@ -3,9 +3,22 @@ from abc import ABCMeta, abstractmethod, abstractproperty
 class Player:
     __metaclass__ = ABCMeta
         
-    def __init__(self, name, colour):
+    def __init__(self, name, colour = None):
         self.name = name
-        self.colour = colour
+        self._colour = colour
+
+    @property
+    def colour(self):
+        return self._colour
+
+    @colour.setter
+    def colour(self, colour):
+        if colour.lower()  in ['white','w']:
+            self._colour = 'white'
+        elif colour.lower()  in ['black','b']:
+            self._colour = 'black'
+        else:
+            raise ValueError("Error: Invalid colour! Must be 'white','w','black' or 'b'.")
 
     @abstractmethod
     def get_move(self, board):
